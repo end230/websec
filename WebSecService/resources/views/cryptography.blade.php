1f5656e5b579426dde7d524d8bc1b9830f2599e7
end230 <thenaughtyboooy@gmail.com>
2025-05-11 14:06:06 +0300

    crypto is done

--- WebSecService/resources/views/cryptography.blade.php
+++ WebSecService/resources/views/cryptography.blade.php
@@ -1,4 +1,6 @@
-<div class="card m-4">
+@extends('layouts.master')
+@section('title', 'Welcome')
+@section('content')<div class="card m-4">
     <div class="card-body">
     <form action="{{route('cryptography')}}" method="get">
     {{ csrf_field() }}
@@ -34,3 +36,4 @@
    </div>
    <div class="card m-4"><div class="card-body"><strong>Result Status:</strong>
    {{$status}}</div></div>
+   @endsection
--- WebSecService/resources/views/layouts/menu.blade.php
+++ WebSecService/resources/views/layouts/menu.blade.php
@@ -19,6 +19,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{route('cryptography')}}">Cryptography</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{route('webcrypto')}}">Web Crypto</a>
+            </li>
             @can('show_users')
             <li class="nav-item">
                 <a class="nav-link" href="{{route('users')}}">Users</a>
